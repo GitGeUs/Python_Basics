2336d0afa80e9aafe185c33b0afaf35390b84686
GitGeUs <108729740+GitGeUs@users.noreply.github.com>
2022-07-30 16:13:32 +0400

    Add files via upload

---
+++ class_01_basic.py
@@ -0,0 +1,10 @@
+
+class Nemo:
+    id = 22
+    color = "yellow"
+    size = "big"
+
+print(Nemo.color)
+
+tt = Nemo()
+print(tt.id)
---
+++ class_calc.py
@@ -0,0 +1,22 @@
+
+class calculator:
+
+    def add (a, b):
+        result = a + b
+        print ("Result is :" + str(result))
+
+    def subtract (a, b):
+        result = a - b
+        print ("Result is :" + str(result))
+
+    def multiply (a, b):
+        result = a * b
+        print ("Result is :" + str(result))
+
+    def division (a, b):
+        result = a / b
+        print ("Result is :" + str(result))
+
+calculator.add(2,4)
+calculator.division(5,3)
+
